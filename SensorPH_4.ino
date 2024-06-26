71995e8b785d6cbec9cfb16ddb9446cf917c9906
rafaeljuniora <160675357+rafaeljuniora@users.noreply.github.com>
2024-06-26 20:54:04 -0300

    Add files via upload

---
+++ SensorPH_4.ino
@@ -0,0 +1,29 @@
+#include <dht.h>
+#include <LiquidCrystal.h> 
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+ 
+void setup()
+{
+  lcd.begin(16, 2);
+}
+ 
+void loop()
+{
+   lcd.clear();
+   lcd.setCursor(3, 0);
+   lcd.print("BIOPARK");
+  lcd.setCursor(3, 1);
+  lcd.print("EDUCACAO");
+  delay(5000); 
+   for (int posicao = 0; posicao < 3; posicao++)
+  {
+    lcd.scrollDisplayLeft();
+    delay(300);
+  }
+   
+   for (int posicao = 0; posicao < 6; posicao++)
+  {
+    lcd.scrollDisplayRight();
+    delay(300);
+  }
+}
